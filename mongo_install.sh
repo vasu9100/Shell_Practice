f64202cf1066902088e7e71b396cbcbc9473c95f
srinivas gonepudi <srigonepudi@gmail.com>
2023-12-14 15:26:21 +0530

    new script added

--- mongo_install.sh
+++ mongo_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Author: Srinivas Gonepudi
-# Created Date : 14-12-2023
-# Description : Installing Mongodb For Roboshop Application
-#Modified date : 14-12-2023
+# Created Date: 14-12-2023
+# Description: Installing MongoDB For RoboShop Application
+# Modified date: 14-12-2023
 
 ID=$(id -u)
 WHO=$(whoami)
@@ -12,10 +12,37 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 N='\033[0m'
 
+validate() {
+    if [ "$1" -ne 0 ]; then
+        echo -e " $2.... ${RED} FAILED"
+        exit 1
+    else
+        echo -e " $2.... ${GREEN} SUCCESS"
+    fi
+}
+
 if [ "$ID" -ne 0 ]; then
-
-    echo -e "${RED} ERROR:: ${N} Your Not a RooT User : Your a ${YELLOW}${WHO}${N} user : Please switch to ${RED}Root${N} User"
+    echo -e "${RED} ERROR:: ${N} You are not a Root User : You are a ${YELLOW}${WHO}${N} user : Please switch to ${RED}Root${N} User"
+    exit 1
 else
+    echo -e "${GREEN} SUCCESS:: ${N} You are now ${YELLOW}${WHO}${N} User. The script will execute shortly: ${GREEN}PLEASE WAIT ${N}"
+fi
 
-    echo -e "${GREEN} SUCCESS:: ${N} Your Now ${YELLOW}${WHO}User So Script Will Exceute Shortly: ${GREEN}PLEASE WAIT ${N}"
-fi      
+mv /home/centos/Shell_Practice/mongo.repo /etc/yum.repos.d
+validate $? "Mongo Repo File Transfer"
+
+echo -e "${YELLOW}Shell is Verifying MongoDB Installation on Server."
+
+dnf list installed | grep -q mongodb-org
+validate $? "Mongo DB is not installed"
+
+echo -e "${YELLOW}Shell is Installing MongoDB."
+
+dnf install mongodb-org -y 
+validate $? "MongoDB-ORG Installation"
+
+systemctl enable mongod
+validate $? "MongoDB Enabled"
+
+systemctl start mongod
+validate $? "MongoDB Started"
