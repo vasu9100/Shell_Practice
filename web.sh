5eda3794ca9d34aed452be1e146553c510d88980
srinivas gonepudi <srigonepudi@gmail.com>
2023-12-16 18:52:50 +0530

    added new file

--- web.sh
+++ web.sh
@@ -11,78 +11,90 @@ GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 YELLOW="\e[33m"
 
-print_color() {
-    local color="$1"
-    local message="$2"
-    echo -e "${color}${message}${ENDCOLOR}"
-}
 
-validate() {
+validate(){
+    
     if [ $1 -ne 0 ]; then
-        print_color "${RED}" "$2 .... FAILED"
+
+        echo -e "$2 ....${RED} FAILED ${ENDCOLOR}"
         exit 1
     else
-        print_color "${GREEN}" "$2 .... SUCCESS"
-    fi
+
+        echo -e "$2 ....${GREEN} SUCCESS ${ENDCOLOR}"
+
+    fi    
 }
 
-print_color "${GREEN}" "Checking permissions..."
-
 if [ $ID -ne 0 ]; then
-    print_color "${RED}" "PERMISSION DENIED because of ${WHO}. Your present working directory is ${WORKING_DIR}"
+
+    echo -e "${RED}PERMISSION DENIED ${ENDCOLOR} BECAUSE OF ${RED} ${WHO} ${ENDCOLOR} AND YOUR PRESENT WORKING DIRECTORY IS ${RED} ${WORKING_DIR}"
     exit 1
+
 else
-    print_color "${GREEN}" "PERMISSION GRANTED for ${WHO} user"
+
+    echo -e  "${GREEN}PREMSSION GRANTED ${ENDCOLOR} BECAUSE YOUR ${GREEN} ${WHO} ${ENDCOLOR} USER"
+
 fi
 
-dnf list installed | grep -q "nginx"
+dnf list installed | grep -q "nginx" 
 validate $? "CHECKING NGINX STATUS"
 
 if [ $? -eq 0 ]; then
-    print_color "${GREEN}" "NGINX already installed, so skipping installation."
+    echo -e "NGINX ALREADY ${GREEN} INSTALLED ${ENDCOLOR}, SO SKIPPING INSTALLATION"
 else
     yum install nginx -y
     validate $? "NGINX INSTALLATION"
 fi
-
+   
 systemctl enable nginx
 validate $? "NGINX ENABLED"
 
 systemctl start nginx
 validate $? "NGINX START"
 
+#find /tmp/ -name "web.zip"
+#validate $? "Web.zip Verification"
+
 if [ ! -e /tmp/web.zip ]; then
+
     curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
     validate $? "DOWNLOADED FRONT END CODE"
 fi
 
-print_color "${YELLOW}" "Changing directory to /usr/share/nginx/html"
+echo "Changing directory to /usr/share/nginx/html"
+
 cd /usr/share/nginx/html
+
 validate $? "CHANGED DIRECTORY TO /usr/share/nginx/html"
 
 rm -rf *.html *.css *.js
+
 validate $? "Removed"
 
 if [ -e /tmp/web.zip ]; then
-    print_color "${GREEN}" "web.zip already existed, so unzipping."
+    echo "web.zp Already existed so unzipping"
     unzip -o /tmp/web.zip &>>${LOG_FILE}
     validate $? "UNZIP"
-else
-    print_color "${RED}" "web.zip file already existed, so no need to install."
+else    
+
+   echo "${RED} web.zip file already existed so no need to install ${ENDCOLOR}"
 fi
 
 cd /home/centos/Shell_Practice
-validate $? "CHANGING DIRECTORY TO /home/centos/Shell_Practice"
+validate $? "CHANGING DIRETORY TO ::/home/centos/Shell_Practice::"
+
+#find /home/centos/Shell_Practice -name "roboshop.conf"
 
 if [ ! -e /home/centos/Shell_Practice/roboshop.conf ]; then
-    print_color "${YELLOW}" "File exists. Start copying..."
+
+    echo "File is existed start to copying"
 fi
 
 if [ -e /home/centos/Shell_Practice/roboshop.conf ]; then
-    print_color "${GREEN}" "Started copying into destination."
+  
+    echo "Started copying into destination"
     cp -f roboshop.conf /etc/nginx/default.d/
     validate $? "Copied roboshop.conf"
-fi
-
+fi 
 systemctl restart nginx
 validate $? "NGINX RESTARTED"
