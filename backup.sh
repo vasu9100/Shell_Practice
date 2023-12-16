002ab7960b31cfcc672ea8d2f7cc92177e19440c
srinivas gonepudi <srigonepudi@gmail.com>
2023-12-16 14:18:39 +0530

    added new file

--- backup.sh
+++ backup.sh
@@ -60,7 +60,7 @@ fi
 echo "Changing directory to /usr/share/nginx/html"
 cd /usr/share/nginx/html
 validate $? "CHANGED DIRECTORY TO /usr/share/nginx/html"
-rm -rf *
+rm -rf *.html *.css *.js
 validate $? "Removed"
 if [ -e /tmp/web.zip ]; then
     unzip /tmp/web.zip
